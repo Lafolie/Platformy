fcfee4c0d2e6c0af90b811d1939abd956e47e3b3
Dale James <lafoliex7@aol.com>
2012-09-18 14:23:33 +0100

    Weapon file.

---
+++ src/weapon/powerbeam.lua
@@ -0,0 +1,9 @@
+-- bulletSpr, spawnOffsetX, spawnOffsetY, damage, cool, fireMode, spawnVelX, spawnVelY
+local spr = "spr/power.spr"
+local spawnOffsetX = 18
+local spawnOffsetY = -9
+local damage = 33
+local cool = 0.1
+local fireMode = "semi"
+
+return { spr, spawnOffsetX, spawnOffsetY, damage, cool, fireMode }
